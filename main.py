3611510acc42a8317a3724fd3ede4bfaca34fab8
Cosmin Alexandru <114618755+getcosmin@users.noreply.github.com>
2024-08-06 10:46:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 ## Promo Tracker - v. 0.10
+
+import pandas as pandas
+from src import promotion
+from src.promo_tracker.tracker import PromotionTracker
+
+
+# Display all table columns
+pandas.set_option('display.max_columns', None)
+
+database = pandas.read_excel('promo_db.xlsx')
+
